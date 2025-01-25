31ae8ed158f9ca19e5bd5d068c1fa477c9c980ac
elniebiet <aniebiet.akpan@gmail.com>
2025-01-25 14:41:51 +0000

    added toolbar resize effect

--- src/templates/sprint_planning.jsx
+++ src/templates/sprint_planning.jsx
@@ -1,10 +1,37 @@
 import * as React from 'react';
 import _templates_toolbar from '../toolbars/templates_toolbar';
+import { useState, useEffect } from 'react';
+
+const _use_window_size = () => {
+    const [window_size, _set_window_size] = useState({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  
+    useEffect(() => {
+      const _handle_resize = () => {
+        _set_window_size({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      };
+  
+      window.addEventListener('resize', _handle_resize);
+      return () => {
+        window.removeEventListener('resize', _handle_resize);
+      };
+    }, []);
+  
+    return window_size;
+};
 
 const _sprint_planning_template = () => {
+    const { width, height } = _use_window_size();
+
     return (
         <div id="sprint_planning_template_root">
-            <_templates_toolbar item_width={35} item_height={35} pos={"top"} />
+            <_templates_toolbar pos={"top"} win_width={width} win_height={height} />
+            <_templates_toolbar pos={"left"} win_width={width} win_height={height} />
         </div>
     );
 };
--- src/toolbars/templates_toolbar.jsx
+++ src/toolbars/templates_toolbar.jsx
@@ -11,25 +11,28 @@ import eraser_img from '../../res/imgs/img_eraser_100x100.png';
 import shapes_img from '../../res/imgs/img_shapes_100x100.png'; 
 import fill_img from '../../res/imgs/img_fill_100x100.png'; 
 
-const _square_fab = styled(Fab)({
-    borderRadius: '8px', 
-    width: '40px',       
-    height: '40px',      
-    backgroundColor: '#ffffff', 
-    '&:hover': { 
-        backgroundColor: '#115293', 
-    },
-});
-
 const _add_toolbar_item = (props) => 
 {
+    let w = props.tb_root_width + 'px';
+    let h = props.tb_root_height + 'px';
+
+    const _square_fab = styled(Fab)({
+        borderRadius: '8px', 
+        width: w,       
+        height: h,      
+        backgroundColor: '#ffffff', 
+        '&:hover': { 
+            backgroundColor: '#115293', 
+        },
+    });
+
     return (
         //TODO: <Draggable>
             <_square_fab>
                 <img 
                     src={props.img_src}
                     alt={props.img_alt_txt} 
-                    style={{ width: props.item_width, height: props.item_height }} 
+                    style={{ width: props.tb_item_width, height: props.tb_item_height }} 
                 />
             </_square_fab>
         // </Draggable>
@@ -43,7 +46,7 @@ let toolbar_styling_top = {
     transform: 'translateX(-50%)', // Offset the div by half its width
     backgroundColor: '#1976d2',
     color: 'white',
-    padding: '10px 20px',
+    padding: '5px 10px',
     borderRadius: '0 0 8px 8px',
     boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
 };
@@ -55,7 +58,7 @@ let toolbar_styling_left = {
     transform: 'translateY(-50%)',
     backgroundColor: '#1976d2', 
     color: 'white',
-    padding: '10px 20px',
+    padding: '5px 10px',
     borderRadius: '0 8px 8px 0',
     boxShadow: '2px 0 6px rgba(0, 0, 0, 0.1)',
 };
@@ -64,6 +67,16 @@ const _templates_toolbar = (props) => {
     let location = props.pos; // ideally top or left
     let flex_dir = "column";
     let toolbar_styling = toolbar_styling_left;
+
+    let ITEM_PERCENTAGE = 0.02; // toolbar item res percentage rtive to window size (2 percent of orig win)
+    let ROOT_PERCENTAGE = 0.03; // toolbar container res percentage rtive to window size (3 percent of orig win)
+
+    let item_width = (ITEM_PERCENTAGE * props.win_width);
+    let item_height = item_width;
+
+    let root_width = (ROOT_PERCENTAGE * props.win_width);
+    let root_height = root_width;
+        
     if(location == "top")
     {
         flex_dir = "row";
@@ -77,14 +90,14 @@ const _templates_toolbar = (props) => {
 
     return (
         <div id="sprint_planning_template_root" style={toolbar_styling}>
-            <Box sx={{ '& > :not(style)': { m: 1 } }} display="flex" flexDirection={flex_dir}>
-                <_add_toolbar_item img_src={pointer_img} img_alt_txt={"Cursor"} item_width={props.item_width} item_height={props.item_height}/>
-                <_add_toolbar_item img_src={sticky_notes_img} img_alt_txt={"Sticky Note"} item_width={props.item_width} item_height={props.item_height}/>
-                <_add_toolbar_item img_src={comment_img} img_alt_txt={"Comment"} item_width={props.item_width} item_height={props.item_height}/>
-                <_add_toolbar_item img_src={board_marker_img} img_alt_txt={"Marker"} item_width={props.item_width} item_height={props.item_height}/>
-                <_add_toolbar_item img_src={shapes_img} img_alt_txt={"Shape"} item_width={props.item_width} item_height={props.item_height}/>
-                <_add_toolbar_item img_src={fill_img} img_alt_txt={"Fill"} item_width={props.item_width} item_height={props.item_height}/>
-                <_add_toolbar_item img_src={eraser_img} img_alt_txt={"Eraser"} item_width={props.item_width} item_height={props.item_height}/>
+            <Box sx={{ '& > :not(style)': { m: 0.5 } }} display="flex" flexDirection={flex_dir}>
+                <_add_toolbar_item img_src={pointer_img} img_alt_txt={"Cursor"} tb_item_width={item_width} tb_item_height={item_height} tb_root_width={root_width} tb_root_height={root_height} />
+                <_add_toolbar_item img_src={sticky_notes_img} img_alt_txt={"Sticky Note"} tb_item_width={item_width} tb_item_height={item_height} tb_root_width={root_width} tb_root_height={root_height}/>
+                <_add_toolbar_item img_src={comment_img} img_alt_txt={"Comment"} tb_item_width={item_width} tb_item_height={item_height} tb_root_width={root_width} tb_root_height={root_height}/>
+                <_add_toolbar_item img_src={board_marker_img} img_alt_txt={"Marker"} tb_item_width={item_width} tb_item_height={item_height} tb_root_width={root_width} tb_root_height={root_height}/>
+                <_add_toolbar_item img_src={shapes_img} img_alt_txt={"Shape"} tb_item_width={item_width} tb_item_height={item_height} tb_root_width={root_width} tb_root_height={root_height}/>
+                <_add_toolbar_item img_src={fill_img} img_alt_txt={"Fill"} tb_item_width={item_width} tb_item_height={item_height} tb_root_width={root_width} tb_root_height={root_height}/>
+                <_add_toolbar_item img_src={eraser_img} img_alt_txt={"Eraser"} tb_item_width={item_width} tb_item_height={item_height} tb_root_width={root_width} tb_root_height={root_height}/>
             </Box>
         </div>
     );
