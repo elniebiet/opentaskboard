611dbeef4b2631b6a8e3533f08fb53bc62e64c04
elniebiet <aniebiet.akpan@gmail.com>
2025-02-10 00:14:51 +0000

    select default cursor type on click of toolbar buttons

--- src/templates/sprint_planning.jsx
+++ src/templates/sprint_planning.jsx
@@ -32,187 +32,201 @@ const _use_window_size = () => {
 };
 
 const _sprint_planning_template = () => {
-    const { width, height } = _use_window_size();
+  const { width, height } = _use_window_size();
 
-    const _handle_drag_over = (e) => {
-      e.preventDefault(); // prevent the default "red stop circle" cursor
-    };
-    
-    /****************** sticky note begins ************************/
-    const [notes, _set_notes] = useState([]);
-    const [note_location, _set_note_location] = useState({loc_x: 100, loc_y: 100}); // last location a note was added
+  const _handle_drag_over = (e) => {
+    e.preventDefault(); // prevent the default "red stop circle" cursor
+  };
+  
+  /****************** sticky note begins ************************/
+  const [notes, _set_notes] = useState([]);
+  const [note_location, _set_note_location] = useState({loc_x: 100, loc_y: 100}); // last location a note was added
 
-    const _add_note = (clicked = true, pos_x = 100, pos_y = 100) => {
-      if(clicked)
-      {
-        // set cursor type
-        _set_cursor_type('default');
-
-        // get last add/drag location
-        const {loc_x, loc_y} = note_location;  
-        let new_loc_x = loc_x + 20;
-        let new_loc_y = loc_y + 20;
-        const new_note = { id: Date.now(), text: "", x_pos: new_loc_x, y_pos: new_loc_y };
-        _set_note_location({loc_x: new_loc_x, loc_y: new_loc_y}); // update last added location
-        _set_notes([...notes, new_note]);
-      }
-      else
-      {
-        // dragged
-        const new_note = { id: Date.now(), text: "", x_pos: pos_x, y_pos: pos_y };
-        _set_notes([...notes, new_note]);
-      }
-    };
-
-    const _delete_note = (id) => {
-      _set_notes(notes.filter((note) => note.id !== id));
-    };
-
-    const _set_note_loc_func = (x, y) => {
-      _set_note_location({loc_x: x, loc_y: y});
-    };
-    /************** sticky note ends ******************************/
-
-    /************** Pointer selection begins **************************/
-    const [cursor_type, _set_cursor_type] = useState("default");
-    
-    const _select_cursor_type = (cursor_type = 'default') => 
+  const _add_note = (clicked = true, pos_x = 100, pos_y = 100) => {
+    if(clicked)
     {
-      _set_cursor_type(cursor_type);
-    };
-    /************** Pointer selection ends ****************************/
+      // set cursor type
+      _set_cursor_type('default');
 
-    /************** Marker drawing begins ************************/
-    const _draw_with_marker = () => 
+      // get last add/drag location
+      const {loc_x, loc_y} = note_location;  
+      let new_loc_x = loc_x + 20;
+      let new_loc_y = loc_y + 20;
+      const new_note = { id: Date.now(), text: "", x_pos: new_loc_x, y_pos: new_loc_y };
+      _set_note_location({loc_x: new_loc_x, loc_y: new_loc_y}); // update last added location
+      _set_notes([...notes, new_note]);
+    }
+    else
     {
-      let cursor_type = `url(${board_marker_img_32}) 10 10, auto`;
-      _set_cursor_type(cursor_type);
+      // dragged
+      const new_note = { id: Date.now(), text: "", x_pos: pos_x, y_pos: pos_y };
+      _set_notes([...notes, new_note]);
+    }
+  };
 
-      // TODO: draw
-    };
+  const _delete_note = (id) => {
+    _set_notes(notes.filter((note) => note.id !== id));
+  };
 
-    /************** Marker drawing ends ************************/
+  const _set_note_loc_func = (x, y) => {
+    _set_note_location({loc_x: x, loc_y: y});
+  };
+  /************** sticky note ends ******************************/
 
-    /************** Add fill begins **********************************/
-    const _add_fill = () => 
-    {
-      let cursor_type = `url(${fill_img_32}) 10 10, auto`;
-      _set_cursor_type(cursor_type);
+  /************** Pointer selection begins **************************/
+  const [cursor_type, _set_cursor_type] = useState("default");
+  
+  const _select_cursor_type = (cursor_type = 'default') => 
+  {
+    _set_cursor_type(cursor_type);
+  };
+  /************** Pointer selection ends ****************************/
 
-      // TODO: fill
-    };
-    /************** Add fill ends **********************************/
+  /************** Marker drawing begins ************************/
+  const _draw_with_marker = () => 
+  {
+    let cursor_type = `url(${board_marker_img_32}) 10 10, auto`;
+    _set_cursor_type(cursor_type);
 
-    /************** Shapes selection begins *********************/
-    const [shapes_sub_toolbar_active, _set_shapes_sub_toolbar_active] = useState(false);
-    const [sub_tb_item_clicked, _set_sub_tb_item_clicked] = useState(false);
-    let initial_click = true;
-    const _show_shape_options = (click_loc_x, click_loc_y) => 
-    {
-      initial_click = true;
-      console.log('shapes selected, button location: ' + click_loc_x, click_loc_y); 
-      _set_shapes_sub_toolbar_active(true);
-      _set_sub_tb_item_clicked(false);
-    };
+    // TODO: draw
+  };
 
-    const _shape_clicked = (e, shape_type) => 
-    {
-      // sub toolbar_item was clicked
-      _set_sub_tb_item_clicked(true);
-    };
+  /************** Marker drawing ends ************************/
 
-    const _deactivate_shapes_sub_tb = () => 
-    {
-      _set_shapes_sub_toolbar_active(false);
-    };
+  /************** Add fill begins **********************************/
+  const _add_fill = () => 
+  {
+    let cursor_type = `url(${fill_img_32}) 10 10, auto`;
+    _set_cursor_type(cursor_type);
 
-    /************** Shapes selection ends *********************/
+    // TODO: fill
+  };
+  /************** Add fill ends **********************************/
 
-    /************** Page listener begins **********************/
-    const _page_click_listener = () => {
-      useEffect(() => {
-          const _handle_page_click = (e) => {
-          console.log("page clicked at:", e.clientX, e.clientY);
-          
-          // listen to page clicks when sub-toolbar is active, incase non of it's buttons were selected
-          if((shapes_sub_toolbar_active === true) && (sub_tb_item_clicked === false))
+  /************** Shapes selection begins *********************/
+  const [shapes_sub_toolbar_active, _set_shapes_sub_toolbar_active] = useState(false);
+  const [sub_tb_item_clicked, _set_sub_tb_item_clicked] = useState(false);
+  let initial_click = true;
+  const _show_shape_options = (click_loc_x, click_loc_y) => 
+  {
+    // set cursor type
+    _set_cursor_type('default');
+
+    initial_click = true;
+    console.log('shapes selected, button location: ' + click_loc_x, click_loc_y); 
+    _set_shapes_sub_toolbar_active(true);
+    _set_sub_tb_item_clicked(false);
+  };
+
+  const _shape_clicked = (e, shape_type) => 
+  {
+    // sub toolbar_item was clicked
+    _set_sub_tb_item_clicked(true);
+  };
+
+  const _deactivate_shapes_sub_tb = () => 
+  {
+    _set_shapes_sub_toolbar_active(false);
+  };
+
+  /************** Shapes selection ends *********************/
+  
+  /************** Add Comment Begins ************************/
+  const _add_comment = () => {
+    // select cursor
+    _set_cursor_type('default');
+    // TODO: Add comment
+    console.log("added comment");
+  };
+  /************** Add Comment Ends **************************/  
+
+  /************** Page listener begins **********************/
+  const _page_click_listener = () => {
+    useEffect(() => {
+        const _handle_page_click = (e) => {
+        console.log("page clicked at:", e.clientX, e.clientY);
+        
+        // listen to page clicks when sub-toolbar is active, incase non of it's buttons were selected
+        if((shapes_sub_toolbar_active === true) && (sub_tb_item_clicked === false))
+        {
+          // sub-toolbar is active, toolbar item not clicked 
+          if(initial_click) // ignore on initial click from listener - we need the next click
           {
-            // sub-toolbar is active, toolbar item not clicked 
-            if(initial_click) // ignore on initial click from listener - we need the next click
-            {
-              initial_click = false;
-            }
-            else  // next click we are interested in
-            {
-              _set_shapes_sub_toolbar_active(false); 
-              initial_click = true;
-            }
+            initial_click = false;
           }
-        };
-    
-        document.addEventListener("click", _handle_page_click);
-    
-        // cleanup function to remove the listener on unmount
-        return () => {
-          document.removeEventListener("click", _handle_page_click);
-        };
-      }, []);
-    
-      return (
-        null
-      );
-    };
-    /**************** Page listener ends **************************/
-
+          else  // next click we are interested in
+          {
+            _set_shapes_sub_toolbar_active(false); 
+            initial_click = true;
+          }
+        }
+      };
+  
+      document.addEventListener("click", _handle_page_click);
+  
+      // cleanup function to remove the listener on unmount
+      return () => {
+        document.removeEventListener("click", _handle_page_click);
+      };
+    }, []);
+  
     return (
-        <div 
-          id="sprint_planning_template_root"
-          onDragOver={_handle_drag_over}
-          style={{ 
-            height: "100vh",
-            width: "100vw",
-            backgroundColor: "#f0f0f0",
-            cursor: cursor_type,
-          }}
-        >
-            <_page_click_listener />
-
-            <_gridlines_normal grid_size={100} line_color="#E6E6E6" z_index={0} />
-            
-            <_templates_toolbar pos={"top"} win_width={width} win_height={height} z_index={50} add_note_func={_add_note} set_note_loc_func={_set_note_loc_func} 
-              select_cursor_func={_select_cursor_type} marker_draw_func={_draw_with_marker} add_fill_func={_add_fill} shapes_selected_func={_show_shape_options} />
-            
-            <_templates_toolbar pos={"left"} win_width={width} win_height={height} z_index={50} add_note_func={_add_note} set_note_loc_func={_set_note_loc_func} 
-              select_cursor_func={_select_cursor_type} marker_draw_func={_draw_with_marker} add_fill_func={_add_fill} shapes_selected_func={_show_shape_options} />
-            
-            {(shapes_sub_toolbar_active === true) && (
-              <_shapes_sub_toolbar shapes_tb_item_clicked_func={_shape_clicked} pos={"top"} win_width={width} win_height={height} z_index={50} deactivate_shapes_sub_tb={_deactivate_shapes_sub_tb} />
-            )}
-
-            {(shapes_sub_toolbar_active === true) && (
-              <_shapes_sub_toolbar shapes_tb_item_clicked_func={_shape_clicked} pos={"left"} win_width={width} win_height={height} z_index={50} deactivate_shapes_sub_tb={_deactivate_shapes_sub_tb} />
-            )}
-
-            <div>
-              <div
-                style={{
-                  top: "20px",
-                  fontSize: "16px",
-                  cursor: "pointer",
-                  position: 'absolute',
-                }}
-              >
-              </div>
-              <div>
-                {notes.map((note) => (
-                  <_sticky_note key={note.id} id={note.id} text={note.text} on_delete={_delete_note} note_update_func={_set_note_loc_func} 
-                    x_pos={note.x_pos} y_pos={note.y_pos} win_width={width} win_height={height}/>
-                ))}
-              </div>
-            </div>
-
-        </div>
+      null
     );
+  };
+  /**************** Page listener ends **************************/
+
+  return (
+      <div 
+        id="sprint_planning_template_root"
+        onDragOver={_handle_drag_over}
+        style={{ 
+          height: "100vh",
+          width: "100vw",
+          backgroundColor: "#f0f0f0",
+          cursor: cursor_type,
+        }}
+      >
+          <_page_click_listener />
+
+          <_gridlines_normal grid_size={100} line_color="#E6E6E6" z_index={0} />
+          
+          <_templates_toolbar pos={"top"} win_width={width} win_height={height} z_index={50} add_note_func={_add_note} set_note_loc_func={_set_note_loc_func} 
+            select_cursor_func={_select_cursor_type} marker_draw_func={_draw_with_marker} add_fill_func={_add_fill} shapes_selected_func={_show_shape_options} 
+            add_comment_func={_add_comment} />
+          
+          <_templates_toolbar pos={"left"} win_width={width} win_height={height} z_index={50} add_note_func={_add_note} set_note_loc_func={_set_note_loc_func} 
+            select_cursor_func={_select_cursor_type} marker_draw_func={_draw_with_marker} add_fill_func={_add_fill} shapes_selected_func={_show_shape_options} 
+            add_comment_func={_add_comment} />
+          
+          {(shapes_sub_toolbar_active === true) && (
+            <_shapes_sub_toolbar shapes_tb_item_clicked_func={_shape_clicked} pos={"top"} win_width={width} win_height={height} z_index={50} deactivate_shapes_sub_tb={_deactivate_shapes_sub_tb} />
+          )}
+
+          {(shapes_sub_toolbar_active === true) && (
+            <_shapes_sub_toolbar shapes_tb_item_clicked_func={_shape_clicked} pos={"left"} win_width={width} win_height={height} z_index={50} deactivate_shapes_sub_tb={_deactivate_shapes_sub_tb} />
+          )}
+
+          <div>
+            <div
+              style={{
+                top: "20px",
+                fontSize: "16px",
+                cursor: "pointer",
+                position: 'absolute',
+              }}
+            >
+            </div>
+            <div>
+              {notes.map((note) => (
+                <_sticky_note key={note.id} id={note.id} text={note.text} on_delete={_delete_note} note_update_func={_set_note_loc_func} 
+                  x_pos={note.x_pos} y_pos={note.y_pos} win_width={width} win_height={height}/>
+              ))}
+            </div>
+          </div>
+
+      </div>
+  );
 };
 
 export default _sprint_planning_template;
--- src/toolbars/templates_toolbar.jsx
+++ src/toolbars/templates_toolbar.jsx
@@ -141,7 +141,11 @@ const _add_toolbar_item = (props) =>
                 props.on_shapes_click(clientX, clientY);
                 break;
             }
-            case TOOLBAR_ITEMS.TBI_COMMENT:                       
+            case TOOLBAR_ITEMS.TBI_COMMENT:
+            {
+                props.on_comment_click();
+                break;    
+            }                       
             default:
             {
                 break;
@@ -268,7 +272,7 @@ const _templates_toolbar = (props) => {
                     tb_item_width={item_width} tb_item_height={item_height} tb_root_width={root_width} tb_root_height={root_height} tb_item_br={item_br} drag_update_func={update_dragged_item_info} 
                     on_stk_click={props.add_note_func} item_loc_update_func={props.set_note_loc_func} />
                     <_add_toolbar_item item_index={TOOLBAR_ITEMS.TBI_COMMENT} tbi_type={TOOLBAR_ITEMS.DRAGGABLE_CLICKABLE} img_src={comment_img} img_alt_txt={"Comment"} 
-                    on_comment_click={_do_nothing} tb_item_width={item_width} tb_item_height={item_height} tb_root_width={root_width} tb_root_height={root_height} tb_item_br={item_br} drag_update_func={update_dragged_item_info} />
+                    on_comment_click={props.add_comment_func} tb_item_width={item_width} tb_item_height={item_height} tb_root_width={root_width} tb_root_height={root_height} tb_item_br={item_br} drag_update_func={update_dragged_item_info} />
                     <_add_toolbar_item item_index={TOOLBAR_ITEMS.TBI_MARKER} tbi_type={TOOLBAR_ITEM_TYPE.CLICKABLE} img_src={board_marker_img} img_alt_txt={"Marker"} 
                     on_marker_click={props.marker_draw_func} tb_item_width={item_width} tb_item_height={item_height} tb_root_width={root_width} tb_root_height={root_height} tb_item_br={item_br} drag_update_func={update_dragged_item_info} />
                     <_add_toolbar_item item_index={TOOLBAR_ITEMS.TBI_SHAPE} tbi_type={TOOLBAR_ITEM_TYPE.CLICKABLE} img_src={shapes_img} img_alt_txt={"Shape"} 
