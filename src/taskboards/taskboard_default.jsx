ea8d7d0629df5e928eee1fcc699842592726e9cc
elniebiet <aniebiet.akpan@gmail.com>
2025-03-02 21:53:09 +0000

    moved note declaration into div

--- src/taskboards/taskboard_default.jsx
+++ src/taskboards/taskboard_default.jsx
@@ -289,12 +289,12 @@ const _taskboard_default = () => {
                 <_draggable_arrow start_pos_x1={arrow.x1_pos} start_pos_y1={arrow.y1_pos} start_pos_x2={arrow.x2_pos} start_pos_y2={arrow.y2_pos} colour={arrow.colour} stroke_width={arrow.stroke_width} />
               ))}
           </div>
-              {/* display note toolbar */}
+          
+          {/* display note toolbar */}
+          <div>
               <_note_toolbar pos={"top"} win_width={width} win_height={height} set_tb_item_loc_func={_set_tb_item_loc_func} taskboard_rerender_func={_trigger_taskboard_rerender} 
                 request_taskboard_state={_request_taskboard_state}
               />
-          <div>
-
           </div>
 
       </div>
