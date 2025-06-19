ca904ac92b0ee8fbea2fe4f2ea3eca9b57ac1b82
elniebiet <aniebiet.akpan@gmail.com>
2025-06-19 16:04:39 +0100

    enforced sticky notes component parameters

--- src/taskboards/components/sticky_note.jsx
+++ src/taskboards/components/sticky_note.jsx
@@ -20,23 +20,41 @@ import { _otbf_update_item_join_arrow_id } from "../../common/otb_finder";
 /**
  * Sticky note component
  */
-const _sticky_note = (props) => {
+const _sticky_note = ({
+    id, 
+    colour, 
+    x_pos, 
+    y_pos, 
+    win_width, 
+    win_height, 
+    win_width_perc, 
+    text, 
+    overall_taskboard_state,
+    main_page_last_click_event_target, 
+    main_page_click_counter, 
+    join_arrow_ids, 
+    tb_item_loc_update_func, 
+    taskboard_rerender_func,
+    highlighted, 
+    show_toolbar, 
+    request_taskboard_state 
+}) => {
 
     const [is_editing, _set_is_editing] = useState(false);
 
     const [z_index, _set_z_index] = useState(_get_max_z_index());
     _use_max_z_index();
     
-    const [complement_colour, _set_complement_colour] = useState(_get_complement_colour(props.colour));
+    const [complement_colour, _set_complement_colour] = useState(_get_complement_colour(colour));
 
     const stk_root_location_ref = useRef(null);
 
     const textarea_ref = useRef(null);
 
-    const [root_div_position, _set_root_div_position] = useState({x: props.x_pos, y: props.y_pos});
+    const [root_div_position, _set_root_div_position] = useState({x: x_pos, y: y_pos});
 
     // cordinates for the actual top-left origin of the note
-    const [overall_top_left, _set_overall_top_left] = useState({x: props.x_pos, y: props.y_pos});
+    const [overall_top_left, _set_overall_top_left] = useState({x: x_pos, y: y_pos});
 
     const [prevent_note_deactivation, _set_prevent_note_deactivation] = useState(false); 
 
@@ -82,7 +100,7 @@ const _sticky_note = (props) => {
         return toolbar_perc;
     };
 
-    let stknote_width = props.win_width_perc * props.win_width;
+    let stknote_width = win_width_perc * win_width;
     stknote_width = ( stknote_width < STKNOTE_MIN_WIDTH ) ? STKNOTE_MIN_WIDTH : stknote_width;
     const font_size = FONT_SIZE_PERC * stknote_width;
     let menubar_item_width   = MENUBAR_ITEM_WIDTH_PERC * stknote_width;
@@ -94,9 +112,9 @@ const _sticky_note = (props) => {
     {
         const {x, y} = _get_note_location_top_left();
         _set_overall_top_left({x: x, y: y});
-        _update_note_loc(props.id, x, y);
-        props.tb_item_loc_update_func(x, y);    // last taskboard item moved location update
-        props.taskboard_rerender_func();
+        _update_note_loc(id, x, y);
+        tb_item_loc_update_func(x, y);    // last taskboard item moved location update
+        taskboard_rerender_func();
     };
 
     const _handle_note_drag_start = (e) => {
@@ -109,17 +127,17 @@ const _sticky_note = (props) => {
         _set_z_index(_get_max_z_index());
         _use_max_z_index();
         _set_is_editing(false);
-        props.taskboard_rerender_func();
+        taskboard_rerender_func();
     }
 
     const _activate_note = (editing_note) => {
         _set_is_editing(editing_note);
         _set_z_index(_get_max_z_index());
         _use_max_z_index();
-        _update_note_active_state(props.id, true);
-        _update_note_highlighted(props.id, true);
-        _update_note_toolbar_show(props.id, true);
-        props.taskboard_rerender_func();
+        _update_note_active_state(id, true);
+        _update_note_highlighted(id, true);
+        _update_note_toolbar_show(id, true);
+        taskboard_rerender_func();
 
         _set_local_param_note_active(true);
     };
@@ -131,10 +149,10 @@ const _sticky_note = (props) => {
         {
             _set_is_editing(false);
             _set_z_index(z_index - 1);
-            _update_note_active_state(props.id, false);
-            _update_note_highlighted(props.id, false);
-            _update_note_toolbar_show(props.id, false);
-            props.taskboard_rerender_func();
+            _update_note_active_state(id, false);
+            _update_note_highlighted(id, false);
+            _update_note_toolbar_show(id, false);
+            taskboard_rerender_func();
 
             _set_local_param_note_active(false);
         }
@@ -146,15 +164,15 @@ const _sticky_note = (props) => {
     };
 
     const _update_text = (updated_text) => { 
-        _update_note_text(props.id, updated_text);
-        props.taskboard_rerender_func();
+        _update_note_text(id, updated_text);
+        taskboard_rerender_func();
     };
 
     const _update_colour = (updated_hex_colour_val) => {
-        _update_note_colour(props.id, updated_hex_colour_val);
+        _update_note_colour(id, updated_hex_colour_val);
         _set_complement_colour(_get_complement_colour(updated_hex_colour_val));
         _set_is_editing(true);
-        props.taskboard_rerender_func();
+        taskboard_rerender_func();
     };
 
     const _colour_picker_btn_clicked = () => { 
@@ -162,9 +180,9 @@ const _sticky_note = (props) => {
     };
 
     const _delete = () => {
-        _update_note_toolbar_show(props.id, false);
-        _delete_note(props.id);
-        props.taskboard_rerender_func();
+        _update_note_toolbar_show(id, false);
+        _delete_note(id);
+        taskboard_rerender_func();
     };
 
     /**
@@ -178,18 +196,18 @@ const _sticky_note = (props) => {
         }
         else
         {
-            return {x: props.x_pos, y: props.y_pos}
+            return {x: x_pos, y: y_pos}
         }
     };
 
     const _toolbar_item_clicked_notif = (item_clicked) => {
         _set_prevent_note_deactivation(true);
-        props.taskboard_rerender_func();
+        taskboard_rerender_func();
     };
 
     const _highlighter_drag_mouse_down = (drag_direction) => {
         _set_prevent_note_deactivation(true);
-        props.taskboard_rerender_func();
+        taskboard_rerender_func();
 
         switch(drag_direction)
         {
@@ -213,12 +231,12 @@ const _sticky_note = (props) => {
     const _highlighter_join_started = (join_position, arrow_id) => {
         _set_prevent_note_deactivation(true);
         // associate arrow id with note
-        _otbf_update_item_join_arrow_id(props.id, join_position, arrow_id, ARROW_JOIN_POINT.START_POINT);
-        props.taskboard_rerender_func();
+        _otbf_update_item_join_arrow_id(id, join_position, arrow_id, ARROW_JOIN_POINT.START_POINT);
+        taskboard_rerender_func();
     };
 
     const _on_mouse_hover = (e) => {
-        if(props.overall_taskboard_state === TASKBOARD_STATES.TBS_JOINING_STARTED)
+        if(overall_taskboard_state === TASKBOARD_STATES.TBS_JOINING_STARTED)
         {
             _set_joining_show_highlighter(true); // show highlighter on hover
         }
@@ -227,7 +245,7 @@ const _sticky_note = (props) => {
     const _highlighter_drag_mouse_up = (drag_direction, width_increase_pixels, height_increase_pixels) => 
     {
         _set_prevent_note_deactivation(true);
-        props.taskboard_rerender_func();
+        taskboard_rerender_func();
 
         switch(drag_direction)
         {
@@ -235,9 +253,9 @@ const _sticky_note = (props) => {
             {
                 // calculate new width and height
                 let new_width = stknote_width + width_increase_pixels;
-                let new_win_width_perc = new_width / props.win_width;
-                _update_note_win_width_perc(props.id, new_win_width_perc);                
-                props.taskboard_rerender_func();
+                let new_win_width_perc = new_width / win_width;
+                _update_note_win_width_perc(id, new_win_width_perc);                
+                taskboard_rerender_func();
 
                 break;
             }
@@ -245,20 +263,20 @@ const _sticky_note = (props) => {
             {
                 // calculate new width and height
                 let new_width = stknote_width + width_increase_pixels;
-                let new_win_width_perc = new_width / props.win_width;
-                _update_note_win_width_perc(props.id, new_win_width_perc);         
+                let new_win_width_perc = new_width / win_width;
+                _update_note_win_width_perc(id, new_win_width_perc);         
 
                 // calculate new top left position
                 let current_root_div_pos = root_div_position;
                 let new_x_pos = current_root_div_pos.x - width_increase_pixels;
                 let new_y_pos = current_root_div_pos.y - height_increase_pixels;
                 _set_root_div_position({x: new_x_pos, y: new_y_pos});
-                _update_note_loc(props.id, new_x_pos, new_y_pos);
+                _update_note_loc(id, new_x_pos, new_y_pos);
 
                 const {x, y} = _get_note_location_top_left();
                 _set_overall_top_left({x: (x - width_increase_pixels), y: (y - height_increase_pixels)});
 
-                props.taskboard_rerender_func();
+                taskboard_rerender_func();
 
                 break;
             }
@@ -266,19 +284,19 @@ const _sticky_note = (props) => {
             {
                 // calculate new width and height
                 let new_width = stknote_width + width_increase_pixels;
-                let new_win_width_perc = new_width / props.win_width;
-                _update_note_win_width_perc(props.id, new_win_width_perc);
+                let new_win_width_perc = new_width / win_width;
+                _update_note_win_width_perc(id, new_win_width_perc);
 
                 // calculate new top left position
                 let current_root_div_pos = root_div_position;
                 let new_x_pos = current_root_div_pos.x - width_increase_pixels;
                 let new_y_pos = current_root_div_pos.y;
                 _set_root_div_position({x: new_x_pos, y: new_y_pos});
-                _update_note_loc(props.id, new_x_pos, new_y_pos);
+                _update_note_loc(id, new_x_pos, new_y_pos);
                 
                 const {x, y} = _get_note_location_top_left();
                 _set_overall_top_left({x: (x - width_increase_pixels), y: y});
-                props.taskboard_rerender_func();
+                taskboard_rerender_func();
 
                 break;
             }
@@ -286,19 +304,19 @@ const _sticky_note = (props) => {
             {
                 // calculate new width and height
                 let new_width = stknote_width + width_increase_pixels;
-                let new_win_width_perc = new_width / props.win_width;
-                _update_note_win_width_perc(props.id, new_win_width_perc);
+                let new_win_width_perc = new_width / win_width;
+                _update_note_win_width_perc(id, new_win_width_perc);
 
                 // calculate new top left position
                 let current_root_div_pos = root_div_position;
                 let new_x_pos = current_root_div_pos.x;
                 let new_y_pos = current_root_div_pos.y - height_increase_pixels;
                 _set_root_div_position({x: new_x_pos, y: new_y_pos});
-                _update_note_loc(props.id, new_x_pos, new_y_pos);
+                _update_note_loc(id, new_x_pos, new_y_pos);
 
                 const {x, y} = _get_note_location_top_left();
                 _set_overall_top_left({x: x, y: (y - height_increase_pixels)});
-                props.taskboard_rerender_func();
+                taskboard_rerender_func();
                 
                 break;
             };
@@ -319,14 +337,14 @@ const _sticky_note = (props) => {
     /********************* Effects block begin ***********************/
     // Subscribe for main page click event
     useEffect(() => {
-        if (stk_root_location_ref.current && !stk_root_location_ref.current.contains(props.main_page_last_click_event_target)) {
+        if (stk_root_location_ref.current && !stk_root_location_ref.current.contains(main_page_last_click_event_target)) {
             if(local_param_note_active === true)
             {
                 // clicked outside of the note component, deactivate note     
-                _deactivate_note(props.main_page_last_click_event_target);
+                _deactivate_note(main_page_last_click_event_target);
             }
         }
-    }, [props.main_page_click_counter]);
+    }, [main_page_click_counter]);
     
     // text area focus on editing
     useEffect(() => {
@@ -334,18 +352,18 @@ const _sticky_note = (props) => {
                 textarea_ref.current.focus();
 
                 // set text to highlight on activation
-                textarea_ref.current.setSelectionRange(props.text.length, props.text.length); // highlight text
+                textarea_ref.current.setSelectionRange(text.length, text.length); // highlight text
             }
         }, [is_editing]
     );
 
     // monitor highlighted param change
     useEffect(() => {
-        if(props.highlighted === false)
+        if(highlighted === false)
         {
             _set_joining_show_highlighter(false); // hide highlighter on hover
         }
-    }, [props.highlighted]);
+    }, [highlighted]);
 
     /********************* Effects block ends ***********************/
 
@@ -359,20 +377,20 @@ const _sticky_note = (props) => {
         >
             {/* display note toolbar */}
             <div>
-                {(props.show_toolbar === true) ? (
-                    <_note_toolbar note_id={props.id} note_width={stknote_width} note_height={stknote_width} 
-                        x_pos={toolbar_x_pos} y_pos={toolbar_y_pos} taskboard_rerender_func={props.taskboard_rerender_func} 
-                        request_taskboard_state={props.request_taskboard_state} note_toolbar_item_clicked={_toolbar_item_clicked_notif}
+                {(show_toolbar === true) ? (
+                    <_note_toolbar note_id={id} note_width={stknote_width} note_height={stknote_width} 
+                        x_pos={toolbar_x_pos} y_pos={toolbar_y_pos} taskboard_rerender_func={taskboard_rerender_func} 
+                        request_taskboard_state={request_taskboard_state} note_toolbar_item_clicked={_toolbar_item_clicked_notif}
                         note_colour_picker_btn_clicked_func={_colour_picker_btn_clicked} note_update_colour_func={_update_colour} 
-                        note_bg_colour={props.colour}
+                        note_bg_colour={colour}
                     />) : (<div></div>)
                 }
             </div>
             {/* display highlighter */}
             <div>
-                {(props.highlighted === true || joining_show_highlighter === true) ? (
+                {(highlighted === true || joining_show_highlighter === true) ? (
                     <_highlighter 
-                        caller_id={props.id} 
+                        caller_id={id} 
                         gap={HIGHLIGHT_PARAMS.highlight_gap} 
                         line_width={HIGHLIGHT_PARAMS.highlight_line_width} 
                         item_top_left_pos={{x: overall_top_left.x, y: overall_top_left.y}} 
@@ -382,10 +400,10 @@ const _sticky_note = (props) => {
                         highlighter_drag_mouse_down={_highlighter_drag_mouse_down}
                         highlighter_drag_mouse_up={_highlighter_drag_mouse_up} 
                         highlighter_join_started={_highlighter_join_started} 
-                        join_arrow_ids={props.join_arrow_ids} 
-                        request_taskboard_state={props.request_taskboard_state} 
-                        overall_taskboard_state={props.overall_taskboard_state} 
-                        taskboard_rerender_func={props.taskboard_rerender_func}
+                        join_arrow_ids={join_arrow_ids} 
+                        request_taskboard_state={request_taskboard_state} 
+                        overall_taskboard_state={overall_taskboard_state} 
+                        taskboard_rerender_func={taskboard_rerender_func}
                         show_top_left_resizer={true}
                         show_top_right_resizer={true}
                         show_bottom_left_resizer={true}
@@ -405,7 +423,7 @@ const _sticky_note = (props) => {
                             style={{
                                 width: stknote_width + 'px',
                                 height: stknote_width + 'px',
-                                backgroundColor: props.colour,
+                                backgroundColor: colour,
                                 padding: "10px",
                                 borderRadius: "8px",
                                 boxShadow: "2px 2px 10px rgba(0,0,0,0.2)",
@@ -428,14 +446,14 @@ const _sticky_note = (props) => {
                                     flexDirection: "row",
                                     gap: (FLEXBOX_GAP_PERC * stknote_width) + 'px',
                                     justifyContent: "flex-end",
-                                    background: props.colour,
+                                    background: colour,
                                 }}
                             >
                                 <div
                                     id="btn_stknote_delete"
                                     style={{ 
                                         flexBasis: menubar_item_width + 'px',
-                                        background: props.colour,
+                                        background: colour,
                                         borderRadius: "50%",
                                         cursor: "pointer",
                                         height: MENUBAR_HGT_PERC * stknote_width + 'px',
@@ -451,7 +469,7 @@ const _sticky_note = (props) => {
                                             margin: "0",
                                             textAlign: "center",
                                             fontWeight: "bold",
-                                            backgroundColor: props.colour,
+                                            backgroundColor: colour,
                                             color: complement_colour,
                                             display: "flex",
                                             alignItems: "center",
@@ -472,7 +490,7 @@ const _sticky_note = (props) => {
                             >
                                 {is_editing ? (
                                     <textarea
-                                        value={props.text}
+                                        value={text}
                                         onChange={(e) => _update_text(e.target.value)}
                                         style={{
                                             marginTop: (FLEXBOX_GAP_PERC * stknote_width) + 'px',
@@ -503,7 +521,7 @@ const _sticky_note = (props) => {
                                         }}
                                         onClick={() => _activate_note(true)}
                                     >
-                                        {props.text}
+                                        {text}
                                     </p>
                                 )}
                             </div>
