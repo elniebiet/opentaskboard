b5cfc44c336a79146a625e477ad7df20d4b109f3
elniebiet <aniebiet.akpan@gmail.com>
2025-06-24 10:29:30 +0100

    added test for all clsid members

--- test/general.test.js
+++ test/general.test.js
@@ -8,10 +8,13 @@ describe("otb id uuid generation function tests", () => {
         expect(result).toBe(null);
     });
 
-    test('test otb arrow id uuid generation', () => {
-        let result = _otb_generate_uuid(COMPONENT_CLSID_PREFIXES.ARROW);
-        console.log(result);
-        expect(typeof result).toBe("string");
+    test('test all clsids - otb arrow id uuid generation', () => {
+        Object.values(COMPONENT_CLSID_PREFIXES).forEach(clsid => {
+            console.log("testing clsid: " + clsid);
+            let result = _otb_generate_uuid(clsid);
+            console.log(result);
+            expect(typeof result).toBe("string");
+        });
     });
 });
 
