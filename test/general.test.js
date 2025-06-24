c11cfd6ca995acc9b882d3751875c28e722914c4
elniebiet <aniebiet.akpan@gmail.com>
2025-06-24 10:52:54 +0100

    added test for randomness of id generator

--- test/general.test.js
+++ test/general.test.js
@@ -10,11 +10,23 @@ describe("otb id uuid generation function tests", () => {
 
     test('test all clsids - otb arrow id uuid generation', () => {
         Object.values(COMPONENT_CLSID_PREFIXES).forEach(clsid => {
-            console.log("testing clsid: " + clsid);
+            // console.log("testing clsid: " + clsid);
             let result = _otb_generate_uuid(clsid);
-            console.log(result);
+            // console.log(result);
             expect(typeof result).toBe("string");
         });
     });
+
+    test('test randomness of id generator', () => {
+        let generated_ids = [];
+        for(let i=0; i<10; i++)
+        {
+            let result = _otb_generate_uuid(COMPONENT_CLSID_PREFIXES.ARROW);
+            expect(generated_ids.includes(result)).toBe(false);
+            generated_ids.push(result);
+        }
+    });
+
+    
 });
 
