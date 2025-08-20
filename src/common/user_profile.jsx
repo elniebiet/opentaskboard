345b010c516afc227582cfeb2eceaa1d3ac915c3
elniebiet <aniebiet.akpan@gmail.com>
2025-08-20 13:59:37 +0100

    added personal details sectino to profile

--- src/common/user_profile.jsx
+++ src/common/user_profile.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from 'react';
+import { URL_MAIN_BACKEND } from './globals';
 import {
   Dialog,
   DialogContent,
@@ -14,6 +15,51 @@ import { _global_state_context } from './global_state_context';
 const _user_profile = ({ trigger_width, trigger_height, img_src }) => {
   const [open, _set_open] = useState(false);
   const { global_email, _set_global_email } = useContext(_global_state_context);
+  const [first_name, _set_first_name] = useState("");
+  const [last_name, _set_last_name] = useState("");
+
+  // fetch personal details 
+  React.useEffect(() => {
+    const _fetch_personal_details = async () => {
+      const access_token = localStorage.getItem("otb_access_token");
+      if (access_token) {
+        let request = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            email: global_email,
+            accessToken: access_token
+          })
+        };
+
+        try {
+          const response = await fetch(`${URL_MAIN_BACKEND}auth/personal_details`, request);
+
+          const data = await response.json();
+
+          if (response.ok && data.statusCode === 200) {
+            _set_first_name(data.personalDetails.firstname);
+            _set_last_name(data.personalDetails.lastname);
+          }
+          else {
+            _set_first_name("");
+            _set_last_name("");
+          }
+        } catch (error) {
+          _set_first_name("");
+          _set_last_name("");
+        }
+      }
+    };
+
+    _fetch_personal_details();
+  }, []);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
 
   return (
     <>
@@ -47,7 +93,7 @@ const _user_profile = ({ trigger_width, trigger_height, img_src }) => {
             src="/static/images/avatar/2.jpg"
             sx={{ width: trigger_width, height: trigger_height, mb: 2, mx: 'auto' }}
           />
-          <Typography variant="h6">Jane Doe</Typography>
+          <Typography variant="h6">{first_name} {last_name}</Typography>
           <Typography variant="body2" color="text.secondary">
             {global_email}
           </Typography>
